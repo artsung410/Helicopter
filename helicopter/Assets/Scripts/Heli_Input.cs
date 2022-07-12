5d3ce345adb62285f8667753872823a512a58cda
artsung410 <61309735+artsung410@users.noreply.github.com>
2022-07-12 09:38:23 +0900

    Update Heli_Input.cs

--- helicopter/Assets/Scripts/Heli_Input.cs
+++ helicopter/Assets/Scripts/Heli_Input.cs
@@ -6,12 +6,6 @@ public class Heli_Input : MonoBehaviour
 {
     public float X { get; private set; }
     public float Y { get; private set; }
-
-    public bool FRONT { get; private set; }
-    public bool BACK { get; private set; }
-    public bool LEFT { get; private set; }
-    public bool RIGHT { get; private set; }
-
     public bool EngineOnOff { get; private set; }
 
 
@@ -21,18 +15,9 @@ public class Heli_Input : MonoBehaviour
         Y = 0f;
 
         EngineOnOff = false;
-        FRONT = false;
-        BACK = false;
-        LEFT = false;
-        RIGHT = false;
 
         X = Input.GetAxis("Horizontal");
         Y = Input.GetAxis("Vertical");
-
-        FRONT = Input.GetKey(KeyCode.I);
-        BACK = Input.GetKey(KeyCode.K);
-        LEFT = Input.GetKey(KeyCode.J);
-        RIGHT = Input.GetKey(KeyCode.L);
 
         EngineOnOff = Input.GetKeyDown(KeyCode.Z);
 
